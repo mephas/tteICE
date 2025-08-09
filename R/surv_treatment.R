1e256748554baf35773861a8512ab1e41afedd3b
Yuhao Deng <61304007+naiiife@users.noreply.github.com>
2025-08-09 08:56:19 -0400

    Update surv_treatment.R

--- R/surv_treatment.R
+++ R/surv_treatment.R
@@ -29,9 +29,6 @@
 #' \item{p.val}{P value of testing the treatment effect based on logrank test.}
 #' }
 #'
-#' @examples
-#' 1
-#'
 #' @details
 #' \describe{
 #' The treatment policy strategy addresses the problem of intercurrent events by expanding
