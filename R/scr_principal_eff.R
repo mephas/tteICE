2e0f3f9bfd8caeec2608b01c76696f582ddd685e
Yuhao Deng <61304007+naiiife@users.noreply.github.com>
2025-08-11 11:30:24 -0400

    Update scr_principal_eff.R

--- R/scr_principal_eff.R
+++ R/scr_principal_eff.R
@@ -1,9 +1,9 @@
 #' @title Fitting the cumulative incidence function using principal stratum strategy
 #'
-#' @description This function estimates the potential cumulative incidence function
-#' based on efficient influence functions using principal stratum strategy (semicompeting
-#' risks data structure). Cox models are employed for survival models. The estimand is defined in a subpopulation where
-#' intercurrent events would never occur regardless of treatment conditions.
+#' @description This function estimates the potential cumulative incidence function based on 
+#' efficient influence functions using principal stratum strategy (semicompeting risks data 
+#' structure). Cox models are employed for survival models. The estimand is defined in a subpopulation 
+#' where intercurrent events would never occur regardless of treatment conditions.
 #'
 #' @param A Treatment indicator, 1 for treatment and 0 for control.
 #'
@@ -45,7 +45,7 @@
 #' \eqn{\tau(t) = P(T(1) < t \mid R(1)=R(0)=\infty) - P(T(0) < t \mid R(1)=R(0)=\infty),}
 #' representing the difference in probabilities of experiencing primary outcome events during \eqn{(0,t)}
 #' under active treatment and placebo in the subpopulation that will not experience intercurrent events
-#' regardless of treatment during \eqn{(0,t)}.
+#' regardless of treatment during \eqn{(0,t)}. A principal ignorability assumption is made for identification.
 #' }
 #'
 #' @seealso \code{\link[ICHe9r1]{scr.principal}}, \code{\link[ICHe9r1]{scr.ICH}}
