62e6b5d1c68cd89163e717785bf70f8515385d46
Yuhao Deng <61304007+naiiife@users.noreply.github.com>
2025-08-16 18:01:40 -0400

    Update basic.R

--- R/basic.R
+++ R/basic.R
@@ -54,7 +54,98 @@
   return(list(Z=A,T=T,R=R,dT=dT,dR=dR,Time=Time,cstatus=cstatus,X=X))
 }
 
-
+.phfit = function(Tr,Dr,Td,Dd,A,X,a){
+  Tr = Tr[A==a]
+  Td = Td[A==a]
+  Dr = Dr[A==a]
+  Dd = Dd[A==a]
+  Dr[Tr==max(Tr)] = 0
+  Dd[Td==max(Td)] = 0
+  tt = sort(unique(c(Td,Tr[Dr==1])))
+  maxt = max(tt)*0.9
+  L = length(tt)
+  N = length(Td)
+  p = ncol(X)
+  if (!is.null(X)) {
+    X = as.matrix(as.matrix(X)[A==a,])
+    p = ncol(X)
+    betad = rep(0, p)
+    betar = rep(0, p)
+  } else {
+    p = 0
+    betad = betar = 0
+  }
+  lamd = rep(1/L, L)
+  lamr = rep(1/L, L)
+  
+  # d
+  eXb = rep(1, N)
+  delta_r = 0
+  iter = 0; tol = 1
+  while(iter<100){
+    iter = iter+1
+    est0 = c(betad,delta_r,lamd[tt<maxt])
+    S0 = sapply(Td, function(l) sum((Td>=l)*eXb*exp(delta_r*Dr*(Tr<l))))
+    if (!is.null(X)){
+      S1 = t(sapply(Td, function(l) colSums((Td>=l)*eXb*exp(delta_r*Dr*(Tr<l))*X)))
+      S2 = t(sapply(Td, function(l) t(X)%*%diag((Td>=l)*eXb*exp(delta_r*Dr*(Tr<l)))%*%X))
+      if (p==1) {S1=t(S1);S2=t(S2)}
+      dbeta = as.numeric(t(X-S1/S0)%*%Dd)
+      ddbeta = t(S1/S0)%*%diag(Dd)%*%(S1/S0) - matrix(colSums(Dd*S2/S0),p,p)
+      betad = betad - ginv(ddbeta) %*% dbeta
+      eXb = exp(as.numeric(X%*%betad))
+    }
+    if (sum(Dd*(Tr<Td))>0) {
+      S1 = sapply(Td, function(l) sum((Td>=l)*eXb*exp(delta_r*Dr*(Tr<l))*Dr*(Tr<l)))
+      ddelta_r = sum(Dd*((Td>Tr)*Dr-S1/S0))
+      dddelta_r = sum(Dd*(S1/S0)^2) - sum(Dd*S1/S0)
+      delta_r = delta_r - ddelta_r/dddelta_r
+    }
+    delta_r = sign(delta_r)*min(abs(delta_r),5)
+    lamd = sapply(tt, function(t) sum(Dd*(Td==t))/sum((Td>=t)*eXb*exp((Tr<t)*delta_r)))
+    lamd[is.nan(lamd)] = 0
+    est = c(betad,delta_r,lamd[tt<maxt])
+    tol = max(abs(est-est0))
+    if (tol<0.00001) break
+  }
+  delta_rd = delta_r
+  
+  # r
+  eXb = rep(1, N)
+  iter = 0; tol = 1
+  while(iter<100){
+    iter = iter+1
+    est0 = c(betar,lamr[tt<maxt])
+    S0 = sapply(Tr, function(l) sum((Tr>=l)*eXb))
+    if (!is.null(X)){
+      S1 = t(sapply(Tr, function(l) colSums((Tr>=l)*eXb*X)))
+      S2 = t(sapply(Tr, function(l) t(X)%*%diag((Tr>=l)*eXb)%*%X))
+      if (p==1) {S1=t(S1);S2=t(S2)}
+      dbeta = as.numeric(t(X-S1/S0)%*%Dr)
+      ddbeta = t(S1/S0)%*%diag(Dr)%*%(S1/S0) - matrix(colSums(Dr*S2/S0),p,p)
+      betar = betar - ginv(ddbeta) %*% dbeta
+      eXb = exp(as.numeric(X%*%betar))
+    }
+    lamr = sapply(tt, function(t) sum(Dr*(Tr==t))/sum((Tr>=t)*eXb))
+    lamr[is.nan(lamr)] = 0
+    est = c(betar,lamr[tt<maxt])
+    tol = max(abs(est-est0))
+    if (tol<0.00001) break
+  }
+  
+  if (!is.null(X)){
+    betad = as.numeric(betad)
+    betar = as.numeric(betar)
+    Xbd = as.numeric(X%*%betad)
+    Xbr = as.numeric(X%*%betar)
+  } else {
+    betad = betar = 0
+    Xbd = Xbr = rep(0,length(Td))
+  }
+  
+  return(list(betad=betad, betar=betar, delta=delta_rd, 
+              Xbd=Xbd, Xbr=Xbr, tt=tt, lamd=lamd, lamr=lamr))
+}
 
 .phfit_d = function(Tr,Dr,Td,Dd,A,X,a,par=NULL){
   X0 = X
