8ddf4b800d866a8f9aa8303407e8106fe579ba30
Yuhao Deng <61304007+naiiife@users.noreply.github.com>
2025-08-16 16:42:27 -0400

    Create riskpred.R

---
+++ R/riskpred.R
@@ -0,0 +1,33 @@
+#' @title Risk prediction
+#'
+#' @description This function predicts the potential cumulative incidence function and treatment effect.
+#'
+#' @param fit An ICH object.
+#'
+#' @param timeset Time to predict the risk.
+#'
+#'
+#' @return A matrix. The meanings of each row are: time points, potential cumulative incidences (under 
+#' treated and under control), treatment effects, standard errors, P-values.
+#' 
+#'
+#' @seealso \code{\link[ICHe9r1]{scr.ICH}}, \code{\link[ICHe9r1]{surv.ICH}}, \code{\link[ICHe9r1]{surv.boot}}
+#'
+#'
+#' @export
+
+riskpredict <- function(fit, timeset, nboot=0, seed=0){
+  fit = surv.boot(fit, nboot, seed)
+  cif1 = .matchy(fit$time,fit$cif1,timeset)
+  cif0 = .matchy(fit$time,fit$cif0,timeset)
+  ate = cif1 - cif0
+  se1 = .matchy(fit$time,fit$se1,timeset)
+  se0 = .matchy(fit$time,fit$se0,timeset)
+  cif1 = .matchy(fit$time,fit$cif1,timeset)
+  se = .matchy(fit$time,fit$se,timeset)
+  p = 2*pnorm(-abs(ate/se))
+  res = rbind(cif1,se1,cif0,se0,ate,se,p)
+  colnames(res) = timeset
+  rownames(res) = c('CIF1','se1','CIF0','se0','ATE','se','p.val')
+  return(res)
+}
