4d75d104291a973029fc88df448b73d7fee16e17
Yuhao Deng <61304007+naiiife@users.noreply.github.com>
2025-09-06 20:40:19 -0400

    Update scr_treatment.R

--- R/scr_treatment.R
+++ R/scr_treatment.R
@@ -56,7 +56,7 @@
 #' \eqn{\tau(t) = P(T(1)) < t) - P(T(0) < t)} as the intention-to-treat analysis.
 #' }
 #'
-#' @seealso \code{\link{scr.treatment.eff}}, \code{\link{scr.ICH}}
+#' @seealso \code{\link[tteICE]{scr.treatment.eff}}, \code{\link[tteICE]{scr.ICH}}
 #'
 #'
 #' @export
