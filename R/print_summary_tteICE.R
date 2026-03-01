c309459601c2e7271f83deba9c0bc14caf968a76
yz-apr <zhouyi0426@hotmail.com>
2026-03-01 10:54:10 +0900

    up new

--- R/print_summary_tteICE.R
+++ R/print_summary_tteICE.R
@@ -2,13 +2,15 @@
 #' @description Print the summary of 'tteICE'
 #' @param x
 #' A fitted object returned by the function \code{tteICE}, \code{surv.tteICE}, or \code{scr.tteICE}.
+#' @param digits
+#' The digits of the results
 #' @param ...
 #' Other arguments in function \code{\link{print.default}}
 #' @method print summary.tteICE
 #' @return Print the summary of a tteICE object
 #' @export
 #'
-print.summary.tteICE <- function(x, ...) {
+print.summary.tteICE <- function(x, digits=3, ...) {
   if(!is.null(x$call)){
     cat("Input:\n")
     print(x$call)
@@ -20,10 +22,10 @@ print.summary.tteICE <- function(x, ...) {
   cat("Estimation method:", x$method, "\n")
   cat("Observations:", x$n, '(including', x$n1, 'treated and', x$n0, 'control)\n')
   cat("Maximum follow-up time:", x$maxt, '\n')
-  cat("P-value of the average treatment effect:", round(p, 3), "\n")
+  cat("P-value of the average treatment effect:", round(p, digits), "\n")
   cat("-----------------------------------------------------------------------\n")
   cat("The estimated cumulative incidences and treatment effects at quartiles:\n")
-  print(round(x$est, 3))
+  print(round(x$est, digits))
   cat("\n")
   invisible(x)
 }
